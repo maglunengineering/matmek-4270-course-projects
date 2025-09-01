cc3f21f146607277177b227c7cfae79fb04bf918
maglunengineering <18057752+maglunengineering@users.noreply.github.com>
2025-09-01 22:35:02 +0200

    Fix differentiate.py

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,24 @@ import numpy as np
 
 
 def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
-    raise NotImplementedError
+    d_n = np.zeros_like(u, dtype=float)
+    d_n[0] = (u[1] - u[0])/dt
+
+    for n in range(1, len(u) - 1):
+        d_n[n] = (u[n+1] - u[n-1]) / (2*dt)
+
+    d_n[-1] = (u[-1] - u[-2])/dt
+
+    return d_n
 
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
-    raise NotImplementedError
+    d_n = np.zeros_like(u)
+    
+    d_n[0] = (u[1] - u[0])/dt
+    d_n[1:-1] = (u[2:] - u[:-2])/(2*dt)
+    d_n[-1] = (u[-1] - u[-2])/dt
+
+    return d_n
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
